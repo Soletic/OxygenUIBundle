45612d52e02edc7c43be0d41234616e2aacd7a0b
Laurent Chedanne <laurent@chedanne.pro>
2013-09-02 18:33:45 +0200

    Habillage formulaire avec Bootstrap

--- OxygenUIBundle.php
+++ OxygenUIBundle.php
@@ -2,8 +2,13 @@
 
 namespace Oxygen\UIBundle;
 
+use Oxygen\UIBundle\DependencyInjection\Compiler\FormRendererCompilerPass;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OxygenUIBundle extends Bundle
 {
+	
 }
--- Resources/public/oxygen/js/ui.js
+++ Resources/public/oxygen/js/ui.js
@@ -10,6 +10,10 @@
 			    // tooltip / popover
 				this.element.find('[data-toggle="tooltip"]').tooltip();
 				this.element.find('[data-toggle="popover"]').popover();
+				// tooltip for asterix required
+				this.element.find('.input-required').each(function() {
+					$(this).tooltip({ placement: 'top' });
+				});
 			} catch (e) {
 				(typeof(console) !== "undefined" && console.log) && console.log(e);
 			}
